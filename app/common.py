b9d09cb1112b2c88329dbd9cfcee609d42120819
MQS123 <15207274447@163.com>
2019-02-23 18:53:45 +0800

    首页显示及分页

---
+++ app/common.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+# author: MQS
+# datetime:2019/2/23 15:19
+# software: PyCharm
+# desc: 公共包
+from flask import Response
+import json
+
+# 返回json结果
+def Result(status, msg, data):
+    result = {
+        'status': status,
+        'msg': msg,
+        'data': data
+    }
+    return Response(json.dumps(result), mimetype='application/json')
--- app/index.py
+++ app/index.py
@@ -5,15 +5,18 @@
 # software: PyCharm
 # desc: 主页
 from run import app
-from flask import render_template
+from flask import render_template, request
 from app.model import *
+from app.common import *
 
-
+# 跳转首页
 @app.route('/', methods=['GET'])
+@app.route('/index', methods=['GET'])
 def index():
-    articles = Article.query.filter().order_by(Article.puthtime.desc()).paginate(1, 5, False)
-    print(type(articles))
-    print(articles.items)
-    for i in articles.items:
-        print(i.title)
-    return render_template('index.html')
+    page = request.args.get('page', 1 , type=int)
+    print(page)
+    try:
+        pagination = Article.query.filter().order_by(Article.puthtime.desc()).paginate(page, per_page=5, error_out=False)
+        return render_template('index.html', articles=pagination.items, pagination=pagination)
+    except Exception:
+        return render_template('common/error.html')
