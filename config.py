c8fe192e517a29e8890f92e68acf5b9c51174c33
MQS123 <15207274447@163.com>
2019-02-27 20:59:16 +0800

    配置了 flask_sqlalchemy的sql打印

--- config.py
+++ config.py
@@ -11,3 +11,4 @@ class Config(object):
     # 格式为mysql+pymysql://数据库用户名:密码@数据库地址:端口号/数据库的名字?数据库格式
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost:3306/buddha_ridge?charset=utf8'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = True
