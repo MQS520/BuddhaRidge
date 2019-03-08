df0fbeaa37e2b10983eecc89596d94264f005bc6
MQS123 <15207274447@163.com>
2019-03-08 22:23:26 +0800

    项目结构重整

--- run.py
+++ run.py
@@ -1,4 +1,26 @@
+from flask import Flask
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
 
-from app import app
+from config import Config
 
-app.run(debug=True,host='0.0.0.0',port=5000)
+app = Flask(__name__)
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.session_protection='strong'
+login_manager.login_view='userRoute.login'
+login_manager.login_message='请登录'
+login_manager.init_app(app)
+
+from app.controller.admin import userRoute
+from app.controller.index import articleRoute
+
+app.register_blueprint(userRoute,url_prefix='')
+app.register_blueprint(articleRoute,url_prefix='')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=5000,debug=True)
