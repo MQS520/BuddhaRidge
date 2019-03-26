f51a582d473e71b2b300dbf33bda46e934b20d6a
MQS <mqs@MQSdeMacBook-Pro.local>
2019-03-26 19:58:59 +0800

    启动ip更改

--- run.py
+++ run.py
@@ -25,4 +25,4 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 if __name__ == '__main__':
-    app.run(debug=True,port=5000,host='127.0.0.1')
+    app.run(debug=True,port=5000,host='0.0.0.0')
